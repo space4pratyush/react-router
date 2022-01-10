68f438e4fd82a9c8b1a961cf0b79268118dd3892
Pratyush <space4pratyush@gmail.com>
2022-01-10 22:38:23 +0530

    working with links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route } from "react-router-dom";
+import MainHeader from "./component/MainHeader";
 import Product from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Product />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Product />
+        </Route>
+      </main>
     </div>
   );
 }
