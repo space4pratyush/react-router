40d7f2c67e6154271f8f66473fef381b1fe570c3
Pratyush <space4pratyush@gmail.com>
2022-01-10 19:54:46 +0530

    defining and using Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route } from "react-router-dom";
+import Product from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Product />
+      </Route>
     </div>
   );
 }
